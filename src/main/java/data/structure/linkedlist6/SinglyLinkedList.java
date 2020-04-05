6437e9b7762341aad02ebc6f037fe18da39395d5
周绍彬 <453220542@qq.com>
2020-04-05 23:55:33 +0800

    单链表

--- src/main/java/data/structure/linkedlist6/SinglyLinkedList.java
+++ src/main/java/data/structure/linkedlist6/SinglyLinkedList.java
@@ -1,4 +1,157 @@
 package data.structure.linkedlist6;
 
 public class SinglyLinkedList {
+
+    private Node head = null;
+
+    public Node findByValue(int value) {
+        Node p = head;
+        while (p != null && p.data != value) {
+            p = p.next;
+        }
+
+        return p;
+    }
+
+    public Node findByIndex(int index) {
+        Node p = head;
+        int pos = 0;
+        while (p != null && pos != index) {
+            p = p.next;
+            ++pos;
+        }
+
+        return p;
+    }
+
+    public void insertToHead(int value) {
+        Node newNode = new Node(value, null);
+        insertToHead(newNode);
+    }
+
+    public void insertToHead(Node newNode) {
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void insertTail(int value) {
+        Node newNode = new Node(value, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node p = head;
+            while (p != null) {
+                p = p.next;
+            }
+            newNode.next = p.next;
+            p.next = newNode;
+        }
+    }
+
+    public void insertAfter(Node p, int value) {
+        Node newNode = new Node(value, null);
+        insertAfter(p, newNode);
+    }
+
+    public void insertAfter(Node p, Node newNode) {
+        if (p == null) {
+            return;
+        }
+
+        newNode.next = p.next;
+        p.next = newNode;
+
+    }
+
+    public void insertBefore(Node p, int value) {
+        Node newNode = new Node(value, null);
+        insertBefore(p, newNode);
+    }
+
+    public void insertBefore(Node p, Node newNode) {
+        if (p == null) {
+            return;
+        }
+
+        if (p == head) {
+            insertToHead(newNode);
+            return;
+        }
+
+        Node q = head;
+        while (q != null && q.next != p) {
+            q = q.next;
+        }
+
+        if (q == null) {
+            return;
+        }
+
+        newNode.next = p;
+        q.next = newNode;
+    }
+
+    public void deleteByNode(Node p) {
+        if (p == null || head == null) {
+            return;
+        }
+
+        if (p == head) {
+            head = head.next;
+            return;
+        }
+
+        Node q = head;
+        while (q != null && q.next != p) {
+            q = q.next;
+        }
+
+        if (q == null) {
+            return;
+        }
+
+        q.next = q.next.next;
+    }
+
+    public void deleteByValue(int value) {
+        if (head == null) {
+            return;
+        }
+
+        Node p = head;
+        Node q = null;
+        while (p != null && p.data != value) {
+            q = p;
+            p = p.next;
+        }
+
+        if (p == null) {
+            return;
+        }
+
+        if(q == null){
+            head = head.next;
+        }else{
+            q.next = q.next.next;
+        }
+    }
+
+    public static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+    }
 }
